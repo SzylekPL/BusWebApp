9e533d05612425e142a17139925788bfd8e3983d
SzylekPL <77230982+SzylekPL@users.noreply.github.com>
2024-05-24 18:58:57 +0200

    Added models and necessary packages.

---
+++ Models/StopPointInfo.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace bus.Models
+{
+	public class StopPointInfo
+	{
+		[JsonProperty("stopPointSymbol")]
+		public string Symbol { get; set; }
+		[JsonProperty("stopPointId")]
+		public int Id { get; set; }
+		[JsonProperty("stopPointName")]
+		public string Name { get; set; }
+		[JsonProperty("departures")]
+		public Departure[] Departures { get; set; }
+	}
+}
